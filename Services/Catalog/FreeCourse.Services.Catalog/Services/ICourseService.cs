9974641400f161c8e3cad781789d4af4ba8d0055
Acer <Acer@LAPTOP-7ABFHQF9>
2023-01-18 00:38:54 +0300

    Proje dosyası ekle.

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Services/ICourseService.cs
@@ -0,0 +1,16 @@
+﻿using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Shared.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Services.Catalog.Services
+{
+    public interface ICourseService
+    {
+        Task<Response<List<CourseDto>>> GetAllAsync();
+        Task<Response<CourseDto>> GetByIdAsync(string id);
+        Task<Response<List<CourseDto>>> GetAllByUserIdAsync(string userid);
+        Task<Response<CourseDto>> CreateAsync(CourseCreateDto courseCreateDto);
+        Task<Response<NoContent>> UpdateAsync(CourseUpdateDto courseUpdateDto);
+        Task<Response<NoContent>> DeleteAsync(string id);
+    }
+}
